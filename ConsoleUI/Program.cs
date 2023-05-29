107e925e644028e40a5d00d20709545721d34c01
YUNUS EMRE ÇELİK <yunusemree60@gmail.com>
2023-05-30 00:27:34 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,9 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.InMemory;
+
+
+ProductManager productManager =new ProductManager(new InMemoryProductDal());
+foreach (var i in productManager.GetAll())
+{
+    Console.WriteLine(i.ProductName);
+}
