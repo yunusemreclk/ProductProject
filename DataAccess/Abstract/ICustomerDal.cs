b92dec5edcca4f6e42bbb96b1e02697bebe4ae3d
YUNUS EMRE ÇELİK <yunusemree60@gmail.com>
2023-06-05 08:07:31 +0300

    Day7

--- DataAccess/Abstract/ICustomerDal.cs
+++ DataAccess/Abstract/ICustomerDal.cs
@@ -1,15 +1,13 @@
 ﻿using Entities.Concrete;
 using System;
 using System.Collections.Generic;
-using System.Dynamic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DataAccess.Abstract
 {
-    public interface IProductDal:IEntityRepository<Product>
+    public interface ICustomerDal:IEntityRepository<Customer>
     {
-       
     }
 }
